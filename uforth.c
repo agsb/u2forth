6b631621d557d9ad42a40665b194c22bbecbc8d1
agsb <barcellos.alvaro@gmail.com>
2020-11-20 17:45:58 -0300

    avr pgmspace addedd

--- uforth.c
+++ uforth.c
@@ -2,9 +2,12 @@
 #include <stdlib.h>
 
 #include <avr/io.h>
+#include <avr/pgmspace.h>
 
 
 /*
+ * u2forth
+ *
  * Alvaro Gomes Sobral Barcellos
  *
  * AVR microcontrolers
@@ -18,27 +21,27 @@
  * model ATMEGA8
  *
  *
-*  Copyright © 2020, Alvaro Barcellos, 
-*  Permission is hereby granted, free of charge, to any person obtaining
-*  a copy of this software and associated documentation files (the
-*  "Software"), to deal in the Software without restriction, including
-*  without limitation the rights to use, copy, modify, merge, publish,
-*  distribute, sublicense, and/or sell copies of the Software, and to
-*  permit persons to whom the Software is furnished to do so, subject to
-*  the following conditions:
-*  
-*  The above copyright notice and this permission notice shall be
-*  included in all copies or substantial portions of the Software.
-*  
-*  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
-*  EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
-*  MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
-*  NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
-*  LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
-*  OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
-*  WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-* 
-*/ 
+ *  Copyright © 2020, Alvaro Barcellos, 
+ *  Permission is hereby granted, free of charge, to any person obtaining
+ *  a copy of this software and associated documentation files (the
+ *  "Software"), to deal in the Software without restriction, including
+ *  without limitation the rights to use, copy, modify, merge, publish,
+ *  distribute, sublicense, and/or sell copies of the Software, and to
+ *  permit persons to whom the Software is furnished to do so, subject to
+ *  the following conditions:
+ *  
+ *  The above copyright notice and this permission notice shall be
+ *  included in all copies or substantial portions of the Software.
+ *  
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ *  EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ *  MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ *  NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+ *  LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+ *  OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+ *  WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ * 
+ */ 
 
 #define CELL_SIZE      2  
 #define RAM_SIZE    1024  
@@ -81,7 +84,6 @@ int forth(void) {
     sp = (uint16_t *) (ram[RAM_SIZE - STACK_SIZE - 2]);
 
 _TWIG: // run opcodes
-
 
     w = rom[ip++];
     
